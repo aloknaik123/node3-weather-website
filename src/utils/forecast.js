ad7cb0a7e8e69aafa720736fca985fcb956bfd22
Alok Naik <abc@abc.com>
2020-12-29 21:11:14 +0000

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const request = require('request')
 
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=54c761837c60e2f8ee8b03873252477a&query=' + latitude + ',' + longitude
 
@@ -9,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
